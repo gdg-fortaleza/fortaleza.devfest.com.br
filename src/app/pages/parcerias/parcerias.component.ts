55f8f08eae0e8715eb74a21d87b5b76bd8c72219
adrianmaycon <mayconlemos1010@gmail.com>
2025-10-09 23:50:54 -0300

    Melhoria banner site header

--- src/app/pages/parcerias/parcerias.component.ts
+++ src/app/pages/parcerias/parcerias.component.ts
@@ -1,18 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-parcerias',
-  templateUrl: './parcerias.component.html',
-  styleUrls: ['./parcerias.component.scss']
+  selector: "app-parcerias",
+  templateUrl: "./parcerias.component.html",
+  styleUrls: ["./parcerias.component.scss"],
 })
 export class ParceriasComponent {
-  mensagemWhatsApp = 'Olá! Tenho interesse em patrocinar o DevFest 2023!'
+  mensagemWhatsApp = "Olá! Tenho interesse em patrocinar o DevFest 2023!";
 
   redirecionarParaEmail() {
-    const email = 'gdgjpb@gmail.com'; // Substitua pelo número de telefone do destinatário
-    const mensagem = 'Tenho interesse em patrocinar o Devfest Nordeste 2023';
-    const assunto = 'Parceria Devfest Nordeste 2023'
+    const email = "gdgfortaleza@gmail.com"; // Substitua pelo número de telefone do destinatário
+    const mensagem = "Tenho interesse em patrocinar o Devfest Fortaleza 2025";
+    const assunto = "Parceria Devfest Fortaleza 2025";
     const linkEmail = `mailto:${email}?subject=${assunto}&body=${mensagem}`;
-    window.open(linkEmail, '_blank');
+    window.open(linkEmail, "_blank");
   }
 }
