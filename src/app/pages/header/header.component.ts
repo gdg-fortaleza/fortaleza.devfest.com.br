c42b53299d880611014cfdeaeac777a6d4ec8051
adrianmaycon <mayconlemos1010@gmail.com>
2025-10-20 08:59:25 -0300

    Melhoria logo

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,32 +1,10 @@
-import { Component, HostListener, Renderer2 } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent {
-  imagePath!: string;
-
-  constructor(private renderer: Renderer2) {
-    this.setImagePath();
-  }
-
-  ngOnInit() {}
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event: any) {
-    console.log(event)
-    this.setImagePath();
-  }
-
-  setImagePath() {
-    const windowWidth = window.innerWidth;
-    
-    if (windowWidth <= 600) {
-      this.imagePath = '/assets/header_mobile.png';
-    } else {
-      this.imagePath = '/assets/header_md.png';
-    }
-  }
+  constructor() {}
 }
